60674f2a59668176dd7d534dee4c9a1f76afa75e
jxsoren <jxsoren@gmail.com>
2025-01-18 13:17:32 -0700

    completed assignment by correclty implementing star patterns

---
+++ CSIS_1400_FoP/labs/first_jgrasp_project/src/Patterns.java
@@ -0,0 +1,26 @@
+public class Patterns {
+    public static void main(String[] argv) {
+        // Initial testing
+        System.out.println("Initial testing");
+        System.out.println("--*--");
+        System.out.print("\n");
+
+        // Diamond of stars
+        System.out.println("Diamond of stars");
+        System.out.println("--*--");
+        System.out.println("-*-*-");
+        System.out.println("*---*");
+        System.out.println("-*-*-");
+        System.out.println("--*--");
+        System.out.print("\n");
+
+        // Diamond of circles
+        System.out.println("Diamond of circles");
+        System.out.println("--o--");
+        System.out.println("-o-o-");
+        System.out.println("o---o");
+        System.out.println("-o-o-");
+        System.out.println("--o--");
+        System.out.print("\n");
+    }
+}
