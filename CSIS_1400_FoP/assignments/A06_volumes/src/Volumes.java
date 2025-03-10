135800ab175c16a933b41f3e45c18e0c1f859496
jxsoren <jxsoren@gmail.com>
2025-03-10 12:18:50 -0600

    CSIS_1400 - Assignment A06 Volumes - init commit

---
+++ CSIS_1400_FoP/assignments/A06_volumes/src/Volumes.java
@@ -0,0 +1,17 @@
+/********************************************** 
+* Author: Josh Sorensen
+*
+* Assignment Volumes calculates the volumes
+* of various shapes.
+***********************************************/
+
+public class Volumes {
+
+   public static double cuboid(double sideA, double sideB, double sideC) {
+      return 0.0; // TODO
+   }
+   
+   public static double cuboid(double side) {
+      return 0.0; // TODO
+   }
+}
