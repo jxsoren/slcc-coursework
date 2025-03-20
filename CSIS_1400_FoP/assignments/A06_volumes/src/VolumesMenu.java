1d1fb4eb7d7a206f838f1d2fcb6b7c0271b090f1
jxsoren <jxsoren@gmail.com>
2025-03-20 17:28:46 -0600

    CSIS_1400 - Assignment A06 Volumes Menu - small refactor and clean up (just need to fix cone volume)

--- CSIS_1400_FoP/assignments/A06_volumes/src/VolumesMenu.java
+++ CSIS_1400_FoP/assignments/A06_volumes/src/VolumesMenu.java
@@ -14,59 +14,54 @@ public class VolumesMenu {
         Scanner input = new Scanner(System.in);
         int selection;
 
-        System.out.printf("cuboid(2, 4, 6): volume = %.1f%n",
-                Volumes.cuboid(2, 4, 6));
-        System.out.printf("cuboid(4): volume = %.1f%n",
-                Volumes.cuboid(4));
-        System.out.printf("ellipsoid(2, 4, 6): volume = %.1f%n",
-                Volumes.ellipsoid(2, 4, 6));
-        System.out.printf("ellipsoid(4): volume = %.1f%n",
-                Volumes.ellipsoid(4));
-        System.out.printf("cylinder(3, 4): volume = %.1f%n",
-                Volumes.cylinder(3, 4));
-        //      System.out.printf("cone(6, 5): volume = %.1f%n",
-        //            Volumes.cone(6, 5));
-
-
         do {
+            menuPrompt();
             selection = input.nextInt();
 
             switch (selection) {
                 case 0:
+                    // If it was 0 print goodbye
                     System.out.println("good bye");
                     break;
                 case 1:
                     // If it was 1 call the 3 parameter method cuboid and display the result; ‘hard-code’ 2, 4, and 6 as  arguments
-                    System.out.print(Volumes.cuboid(2, 4, 6));
+                    System.out.printf("cuboid(2, 4, 6): volume = %.1f%n",
+                            Volumes.cuboid(2, 4, 6));
                     break;
                 case 2:
                     // If it was 2 call the 1 parameter method cuboid and display the result; ‘hard-code’ the value 4 as argument
-                    System.out.print(Volumes.cuboid(4));
+                    System.out.printf("cuboid(4): volume = %.1f%n",
+                            Volumes.cuboid(4));
                     break;
                 case 3:
                     // If it was 3 call the 3 parameter method ellipsoid and display the result; ‘hard-code’ 2, 4, and 6 as  arguments
-                    System.out.print(Volumes.ellipsoid(2, 4, 6));
+                    System.out.printf("ellipsoid(2, 4, 6): volume = %.1f%n",
+                            Volumes.ellipsoid(2, 4, 6));
                     break;
                 case 4:
                     // If it was 4 call the 1 parameter method ellipsoid and display the result; ‘hard-code’ the value 4 as argument
-                    System.out.print(Volumes.ellipsoid(4));
+                    System.out.printf("ellipsoid(4): volume = %.1f%n",
+                            Volumes.ellipsoid(4));
                     break;
                 case 5:
                     // If it was 5 call the method cylinder and display the result; ‘hard-code’ 3 and 4 as arguments
-                    System.out.print(Volumes.cylinder(3, 4));
+                    System.out.printf("cylinder(3, 4): volume = %.1f%n",
+                            Volumes.cylinder(3, 4));
                     break;
                 case 6:
                     // If it was 6 call the method cone and display the result;‘hard-code’ 6 and 5 as arguments
                     // Todo: fix the cone method to properly return the correct volume of a cone
-                    System.out.print(Volumes.cone(6, 5));
+                    System.out.printf("cone(6, 5): volume = %.1f%n",
+                            Volumes.cone(6, 5));
                     break;
                 default:
+                    // If it was any other number print invalid input
                     System.out.println("invalid number");
                     break;
             }
 
+            System.out.println();
         } while (selection != 0);
-
     }
 
     public static void menuPrompt() {
@@ -75,7 +70,7 @@ public class VolumesMenu {
         System.out.printf("%-25s %s%n",
                 "3 .. ellipsoid(2, 4, 6)", "4 .. ellipsoid(4)");
         System.out.printf("%-25s %s%n",
-                "5 .. cylinder(3,4)", "6 .. cone(6, 5)");
+                "5 .. cylinder(3, 4)", "6 .. cone(6, 5)");
         System.out.println("0 .. exit");
         System.out.print("Your choice: ");
     }
