c5373d45c502f3819f29da3737064d10f51660ba
jxsoren <jxsoren@gmail.com>
2025-04-04 21:55:31 -0600

    CSIS_1400 - Assignment A07 Dice - Completed

---
+++ CSIS_1400_FoP/assignments/A07_dice/README.md
@@ -0,0 +1,29 @@
+## Instructions:
+
+Create a new Java file called `Dice.java`
+
+- Simulate the rolling of 2 dice.
+  Do that by creating one single object of type Random and by reusing it to roll 
+  the first die and then the second die.
+- Once both dice have been rolled calculate the sum of the two values.
+- Use a one-dimensional integer array to count how often each sum appears.
+  When rolling two dice the sum will be a value from 2 – 12. However, not every 
+  sum has the same probability of being rolled.
+  E.g.: There are three ways to roll a sum of 4, six ways to roll a sum of 7 but only 
+  one way to roll a sum of 12.
+- Roll the two dice 36,000 times.
+- Display the results in a tabular format like in the sample output. Start with a 
+  header line (Sum, Frequency, and Percentage) and list the numbers in right 
+  aligned columns.
+  The percentage should display only one digit after the decimal point.
+  In order to output % within a format string write %%
+
+Tip: Check whether the results are reasonable (e.g. the sum 7 should be rolled 
+about 1/6 of the time)
+
+## Video Recording Instructions:
+Create a **video recording** that is **40 - 80 seconds** long. It should include the following:
+* A **title page** that lists the name of the assignment and the names of all who contributed to the code.
+* **Your code.** Calmly scroll from top to bottom. Maximize the height of the editor window to minimize scrolling and avoid scrolling back up.
+* The **running program** After you have shown the code, run the program. Adjust the size of the output window to provide enough room for the output.
+
---
+++ CSIS_1400_FoP/assignments/A07_dice/src/Dice.java
@@ -0,0 +1,59 @@
+/**********************************************
+ * Author: Josh Sorensen
+ *
+ * Assignment: A07 - Dice
+ ***********************************************/
+
+import java.util.Random;
+import java.util.Arrays;
+
+public class Dice {
+    private static final Random random = new Random();
+    private static final int NUMBER_OF_ROLLS = 36_000;
+
+    // Each index represents the dice roll sum (excluding 0 and 1)
+    // index 2 = dice roll sum of 2
+    // index 7 = dice roll sum of 7
+    // index 12 = dice roll sum of 12
+    // etc.
+    private static final int[] rollFrequencyCounts = new int[13];
+
+    public static void main(String[] args) {
+        // Roll the two dice 36k times
+        for (int i = 0; i < NUMBER_OF_ROLLS; i++) {
+            int rollSum = diceRollSum();
+            rollFrequencyCounts[rollSum]++;
+        }
+
+        System.out.println();
+        displayResults();
+        System.out.println();
+    }
+
+    // Helper Methods
+
+    private static int diceRollSum() {
+        int roll1 = random.nextInt(1, 7);
+        int roll2 = random.nextInt(1, 7);
+
+        return roll1 + roll2;
+    }
+
+    private static void displayResults() {
+        String[] headers = {"Sum", "Frequency", "Percentage"};
+        for (String header : headers) {
+            System.out.printf("%s  ", header);
+        }
+
+        for (int i = 2; i < rollFrequencyCounts.length; i++) {
+            int rollFrequency = rollFrequencyCounts[i];
+            double frequencyPercent = frequencyPercentage(rollFrequency);
+            System.out.printf("%n%3d%11d%11.1f%%", i, rollFrequency, frequencyPercent);
+        }
+    }
+
+    private static double frequencyPercentage(int rollCount) {
+        double decimal = rollCount / (double) NUMBER_OF_ROLLS;
+        return decimal * 100; // move decimal point over 2 digits
+    }
+}
