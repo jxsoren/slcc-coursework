67396d2369c95fe2a3a60a7b6690b648e7cc590d
jxsoren <jxsoren@gmail.com>
2025-05-21 17:59:13 -0600

    CSIS 2440 - Module 1 | Assignment 1 | Happy Birthday - Completed

---
+++ CSIS_2440_WP/assignments/01-birthday/birthday/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CSIS 2440 - Mod 1 | Assignment 1</title>
+</head>
+
+<body>
+<?php
+    echo "<h1>Happy Birthday!</h1>";
+    echo "<img src='img/frosty-the-snowman.png' alt='frosty-the-snowman' />";
+?>
+</body>
+</html>
