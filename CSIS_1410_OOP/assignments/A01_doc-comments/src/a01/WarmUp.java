9d24ffa1cb842f65905a077ce208e2d14d5015e7
jxsoren <jxsoren@gmail.com>
2025-05-28 15:40:17 -0600

    CSIS 1410 | Assignment A01 - Doc Comments | Enhanced boilerplate for tests and Javadoc comments

--- CSIS_1410_OOP/assignments/A01_doc-comments/src/a01/WarmUp.java
+++ CSIS_1410_OOP/assignments/A01_doc-comments/src/a01/WarmUp.java
@@ -11,9 +11,55 @@ import java.util.ArrayList;
 public class WarmUp {
     public static void main(String[] args) {
 
+        // !---- Review Objects ----!
+        System.out.println("Review Objects");
+
+        // 1. Distance
+
+        Point onePoint1A = new Point(3, 4);
+        Point onePoint2A = new Point(3, 6);
+        double distanceResultA = distance(onePoint1A, onePoint2A);
+
+        System.out.printf("Distance between p1 %s and p2 %s is %s", onePoint1A, onePoint2A, distanceResultA);
+
+        Point onePoint1B = new Point(3, 4);
+        Point onePoint2B = new Point(3, 6);
+        double distanceResultB = distance(onePoint1B, onePoint2B);
+
+        System.out.printf("Distance between p1 %s and p2 %s is %s", onePoint1B, onePoint2B, distanceResultB);
+
+        // 2. Greatest Distance From Origin
+
+        Point twoPoint1A = new Point(4, 3);
+        Point twoPoint2B = new Point(3, -1);
+        Point twoPoint3B = new Point(3, 4);
+        double greatestDistanceFromOriginResultA = greatestDistanceFromOrigin(twoPoint1A, twoPoint2B, twoPoint3B);
+
+        System.out.printf("Both p1 (4, 3) and p3 (3, -4) have the same distance of 5 from the origin. Since p2 is closer to the origin, the greatest distance from the origin is 5.");
+
+        // 3. Area
+
+        // !---- Review Arrays ----!
+        System.out.println("Review Arrays");
+
+        // 4. First Eight Multiples
+
+        // 5. First And Last To XXX
+
+        // 6. Content
+
+        // !---- Review ArrayList ----!
+        System.out.println("Review ArrayList");
+
+
+        // 7. Hyphenate
+
+        // 8. More Of The Same
+
+        // 9. Take
     }
 
-    // Review Objects
+    // !---- Review Objects ----!
 
     /**
      * This method returns the distance between p1 and p2.
@@ -51,8 +97,8 @@ public class WarmUp {
      * This method returns the area of the rectangle that extends from the origin along the x-axis and y-axis to the point p, as shown below.
      * If p is located on the x-axis or y-axis, the length or width is 0, and the method should return 0.
      *
-     * @param p A Point
-     * @return returns the area of the rectangle that extends from the origin along the x-axis and y-axis to the point p.
+     * @param p point // Todo - write a better param description
+     * @return // Todo - write return type here
      */
     public static double area(Point p) {
         // Todo: Implement Test
@@ -62,14 +108,14 @@ public class WarmUp {
         return 0d;
     }
 
-    // Review Arrays
+    // !---- Review Arrays ----!
 
     /**
      * This method returns an array that includes the first eight multiples of n.
-     * Those are 1*n, 2*n, .. 8*n
+     * Those are 1*n, 2*n, 8*n
      *
-     * @param n number (coordinate on an XY graph)
-     * @return the distance between p1 and p2.
+     * @param n number // Todo - write a better param description
+     * @return // Todo - write return type here
      */
 
     public static int[] firstEightMultiples(int n) {
@@ -86,9 +132,7 @@ public class WarmUp {
      * However, if the array includes one or more elements, the first and last elements might need to be exchanged.
      * The examples below show the array strings before and after the method call.
      *
-     * @param p1 Point 1 (coordinate on an XY graph)
-     * @param p2 Point 2 (coordinate on an XY graph)
-     * @return the distance between p1 and p2.
+     * @param strings Array of Strings // Todo - write a better param description
      */
 
     public static void firstAndLastToXXX(String[] strings) {
@@ -102,9 +146,8 @@ public class WarmUp {
      * If the array is empty, an empty string should be returned. The array provided as arguments must not be changed.
      * The examples below show the array points that is passed to the method and the string that should be returned.
      *
-     * @param p1 Point 1 (coordinate on an XY graph)
-     * @param p2 Point 2 (coordinate on an XY graph)
-     * @return the distance between p1 and p2.
+     * @param points Array of Point objects // Todo - write a better param description
+     * @return // Todo - write return type here
      */
 
     public static String content(Point[] points) {
@@ -115,14 +158,16 @@ public class WarmUp {
         return new String();
     }
 
+    // !---- Review ArrayList ----!
+
+
     /**
      * This method creates a string that includes all syllables of the list separated by dashes.
      * If the list is empty, an empty string should be returned.
      * The lists provided as arguments must not be changed in the process.
      *
-     * @param p1 Point 1 (coordinate on an XY graph)
-     * @param p2 Point 2 (coordinate on an XY graph)
-     * @return the distance between p1 and p2.
+     * @param syllables ArrayList of syllable strings // Todo - write a better param description
+     * @return // Todo - write return type here
      */
 
     // ArrayList
@@ -140,9 +185,8 @@ public class WarmUp {
      * The second string has length 2 and includes the character twice, etc.
      * The fifth string has length 5 and includes the character five times.
      *
-     * @param p1 Point 1 (coordinate on an XY graph)
-     * @param p2 Point 2 (coordinate on an XY graph)
-     * @return the distance between p1 and p2.
+     * @param c a character // Todo - write a better param description
+     * @return // Todo - write return type here
      */
 
     public static ArrayList<String> moreOfTheSame(char c) {
@@ -153,15 +197,18 @@ public class WarmUp {
         return new ArrayList<String>();
     }
 
+    // Good template to use:
+
     /**
-     * This method creates and returns a new list that includes the first n points from the list provided as an argument.
-     * The list provided as arguments should not be changed.
-     * If n is 0, an empty list should be returned.
-     * If n is negative or n is greater than the length of the list, an IllegalArgumentException should be thrown.
+     * Creates and returns a new list that includes the first n points from the list
+     * provided as an argument. The original list is not modified.
+     * <p>
+     * If n is 0, an empty list is returned.
      *
-     * @param p1 Point 1 (coordinate on an XY graph)
-     * @param p2 Point 2 (coordinate on an XY graph)
-     * @return the distance between p1 and p2.
+     * @param list the ArrayList of Point objects from which to take elements
+     * @param n    the number of elements to take from the beginning of the list
+     * @return a new ArrayList containing the first n Point objects from the input list
+     * @throws IllegalArgumentException if n is negative or greater than the list size
      */
 
     public static ArrayList<Point> take(ArrayList<Point> list, int n) {
